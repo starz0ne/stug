6ab874daf0c8eecf9f2c0d092ccc08df26f483ca
spaceu2ii <spaceuxc@gmail.com>
2023-07-27 19:22:29 -0400

    add back inject.html

--- ui-dev/script.js
+++ ui-dev/script.js
@@ -1,107 +1,106 @@
-// shortcut functions
-function idget(e) { return document.getElementById(e); }
-function qrget(e) { return document.querySelector(e); }
-function cdrce(e, v) { document.documentElement.style.setProperty(e, v); }
-function ggcrv(e) { return getComputedStyle(document.documentElement).getPropertyValue(e).trim(); }
+// define shortcut functions
+const idget = (e) => document.getElementById(e);
+const qrget = (e) => document.querySelector(e);
+const cdrce = (e, v) => document.documentElement.style.setProperty(e, v);
+const ggcrv = (e) => getComputedStyle(document.documentElement).getPropertyValue(e).trim();
 
-// drag variables
-let offsetX, offsetY, isDragging = false;
+// initialize some variables
+let initialX, initialY, mainbordercolor, headerbordercolor, headercolor;
 
-// other variables
-var oldmaincolor;
-
-// drag functions
-function handleMouseDown(event) {
-   isDragging = true;
-   const boundingRect = modmenu.getBoundingClientRect();
-   offsetX = event.clientX - boundingRect.left;
-   offsetY = event.clientY - boundingRect.top;
-}
-function handleMouseMove(event) {
-   if (!isDragging) return;
-   const newX = event.clientX - offsetX;
-   const newY = event.clientY - offsetY;
-   modmenu.style.left = `${newX}px`;
-   modmenu.style.top = `${newY}px`;
-}
-function handleMouseUp() { isDragging = false; }
-
-// inactive/active toggle handler
-function handleToggle(element) {
-   let ev = element.value;
-   let isActive = (ev === "INACTIVE") ? false : true;
-   if (!isActive) {
-      element.className = 'toggle active';
-      element.value = "ACTIVE";
-   } else {
-      element.className = 'toggle inactive';
-      element.value = "INACTIVE";
-   }
+// function for handling inactive/active toggle button
+const handleToggle = (element) => {
+  const ev = element.value;
+  const isActive = (ev === "INACTIVE") ? false : true;
+  if (!isActive) {
+    element.className = 'toggle active';
+    element.value = "ACTIVE";
+  } else {
+    element.className = 'toggle inactive';
+    element.value = "INACTIVE";
+  }
 };
 
-// minimize/maximize window functions
-function minimizeModMenu() {
-   // change dimensions
-   cdrce('--modmenu-header-height', '5px');
-   cdrce('--modmenu-height', '28px');
-   cdrce('--modmenu-width', '33px');
+// functions for handling the minimize/maximize button
+const minimizeModMenu = () => {
+  /// change dimensions
+  cdrce('--modmenu-header-height', '5px');
+  cdrce('--modmenu-height', '28px');
+  cdrce('--modmenu-width', '33px');
 
-   // change colors
-   mainbordercolor = ggcrv('--main-border-color');
-   headerbordercolor = ggcrv('--header-border-color');
-   headercolor = ggcrv('--header-color');
-   cdrce('--main-border-color', 'var(--minimized-color)');
-   cdrce('--header-border-color', 'var(--minimized-color)');
-   cdrce('--header-color', 'var(--minimized-color)');
+  /// change colors
+  mainbordercolor = ggcrv('--main-border-color');
+  headerbordercolor = ggcrv('--header-border-color');
+  headercolor = ggcrv('--header-color');
+  cdrce('--main-border-color', 'var(--minimized-color)');
+  cdrce('--header-border-color', 'var(--minimized-color)');
+  cdrce('--header-color', 'var(--minimized-color)');
 
-   // change visibility and positions
-   cdrce('--maximize-button-left', '165px');
-   cdrce('--visibility', 'hidden');
+  /// change visibility and positions
+  cdrce('--maximize-button-left', '165px');
+  cdrce('--visibility', 'hidden');
 
-   // hide the minimize element & show the maximize element
-   maximize.className = 'maximize';
-   minimize.className = 'minimize hidden';
-}
-function maximizeModMenu() {
-   // hide the maximize element & show the minimize element
-   maximize.className = 'maximize hidden';
-   minimize.className = 'minimize';
+  /// hide the minimize element & show the maximize element
+  maximize.className = 'maximize';
+  minimize.className = 'minimize hidden';
+};
 
-   // change visibility and positions
-   cdrce('--visibility', 'visible');
-   cdrce('--maximize-button-left', '0px');
+const maximizeModMenu = () => {
+  /// hide the maximize element & show the minimize element
+  maximize.className = 'maximize hidden';
+  minimize.className = 'minimize';
 
-   // change colors
-   cdrce('--main-border-color', mainbordercolor);
-   cdrce('--header-border-color', headerbordercolor);
-   cdrce('--header-color', headercolor);
+  /// change visibility and positions
+  cdrce('--visibility', 'visible');
+  cdrce('--maximize-button-left', '0px');
 
-   // change dimensions
-   cdrce('--modmenu-width', '340px');
-   cdrce('--modmenu-height', '300px');
-   cdrce('--modmenu-header-height', '8px');
-}
+  /// change colors
+  cdrce('--main-border-color', mainbordercolor);
+  cdrce('--header-border-color', headerbordercolor);
+  cdrce('--header-color', headercolor);
+
+  /// change dimensions
+  cdrce('--modmenu-width', '340px');
+  cdrce('--modmenu-height', '300px');
+  cdrce('--modmenu-header-height', '8px');
+};
 
 // exploit functions
-function anonymityMode() {
+const anonymityMode = () => {
 
-}
+};
 
-// get modmenu elements
-var modmenuheader = qrget('.modmenu-header');
-var modmenu = qrget('.modmenu');
-var minimize = idget('mini');
-var maximize = idget('maxi');
+// variables for the modmenu elements
+const modmenuheader = qrget('.modmenu-header');
+const modmenu = qrget('.modmenu');
+const minimize = idget('mini');
+const maximize = idget('maxi');
 
-// add drag function listeners
-modmenuheader.addEventListener('mousedown', handleMouseDown);
-document.addEventListener('mousemove', handleMouseMove);
-document.addEventListener('mouseup', handleMouseUp);
+// variables for modmenu feature elements
+const anonymitymode = idget('am-m');
+
+// event listeners for dragging
+modmenuheader.addEventListener("dragstart", function(event) {
+  /// store initial position when drag starts
+  initialX = event.clientX - modmenuheader.offsetLeft;
+  initialY = event.clientY - modmenuheader.offsetTop;
+});
+
+modmenuheader.addEventListener("drag", function(event) {
+  /// update position as the drag is happening
+  var newX = event.clientX - initialX;
+  var newY = event.clientY - initialY;
+
+  /// apply new position
+  modmenuheader.style.left = newX + "px";
+  modmenuheader.style.top = newY + "px";
+});
 
 // process interactions with minimize/maximize buttons
-minimize.onclick = function () { minimizeModMenu(); }
-maximize.onclick = function () { maximizeModMenu(); }
+minimize.onclick = () => minimizeModMenu();
+maximize.onclick = () => maximizeModMenu();
 
-// get modmenu feature elements, and process interactions for them
-var anonymitymode = idget('am-m');
-anonymitymode.onclick = function() { handleToggle(this); anonymityMode(); };
+// process interactions for modmenu features
+anonymitymode.onclick = function() {
+  handleToggle(this);
+  anonymityMode();
+};
