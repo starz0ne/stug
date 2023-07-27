5cdedc52c25b83fed78fba0f9e682f21a24869c0
spaceu2ii <spaceuxc@gmail.com>
2023-07-27 17:27:52 -0400

    idk why vscode is forcing me to write these messages

--- userscript.js
+++ userscript.js
@@ -12,108 +12,145 @@
 (function() {
 'use strict';
 
-   // modmenu html/css
-   let modmenuHTML = ``;
-   let modmenuCSS = `<link href="https://github.com/starz0ne/stug/blob/5870375f370f4dc09bf77b59c2738ad78e28eb03/ui-dev/style.css" rel="stylesheet"/>`;
+// modmenu html/css
+let modmenuHTML, modmenuCSS;
+modmenuCSS = `<link rel="stylesheet" href="https://raw.githubusercontent.com/starz0ne/stug/master/ui-dev/style.css">`;
+fetch('https://raw.githubusercontent.com/starz0ne/stug/master/ui-dev/page.html')
+   .then(response => response.text())
+   .then(html => {
+      modmenuHTML = html;
+   })
+   .catch(error => {
+      console.error('Error fetching HTML:', error);
+   });
 
-   // insert modmenu into page
-   const sE = document.createElement('style');
-   const dE = document.createElement('div');
-   dE.innerHTML = modmenuHTML;
-   sE.innerHTML = modmenuCSS;
-   document.head.appendChild(sE);
-   document.body.appendChild(dE);
+// insert modmenu into page
+const sE = document.createElement('style');
+const dE = document.createElement('div');
+dE.innerHTML = modmenuHTML;
+sE.innerHTML = modmenuCSS;
+document.head.appendChild(sE);
+document.body.appendChild(dE);
 
-   // shortcut functions
-   function idget(e) { return document.getElementById(e); }
-   function qrget(e) { return document.querySelector(e); }
-   function cdrce(e, v) { document.documentElement.style.setProperty(e, v); }
+// shortcut functions
+function idget(e) { return document.getElementById(e); }
+function qrget(e) { return document.querySelector(e); }
+function cdrce(e, v) { document.documentElement.style.setProperty(e, v); }
+function ggcrv(e) { return getComputedStyle(document.documentElement).getPropertyValue(e).trim(); }
 
-   // drag variables
-   let offsetX, offsetY, isDragging = false;
+// drag variables
+let offsetX, offsetY, isDragging = false;
 
-   // drag functions
-   function handleMouseDown(event) {
-      isDragging = true;
-      const boundingRect = modmenu.getBoundingClientRect();
-      offsetX = event.clientX - boundingRect.left;
-      offsetY = event.clientY - boundingRect.top;
-   } function handleMouseMove(event) {
-      if (!isDragging) return;
-      const newX = event.clientX - offsetX;
-      const newY = event.clientY - offsetY;
-      modmenu.style.left = `${newX}px`;
-      modmenu.style.top = `${newY}px`;
-   } function handleMouseUp() { isDragging = false; }
+// other variables
+var oldmaincolor;
 
-   // inactive/active toggle handler
-   function handleToggle(element) {
-      let ev = element.value;
-      let isActive = (ev === "INACTIVE") ? false : true;
-      if (!isActive) {
-         element.className = 'toggle active';
-         element.value = "ACTIVE";
-      } else {
-         element.className = 'toggle inactive';
-         element.value = "INACTIVE";
-      }
-   };
+// drag functions
+function handleMouseDown(event) {
+   isDragging = true;
+   const boundingRect = modmenu.getBoundingClientRect();
+   offsetX = event.clientX - boundingRect.left;
+   offsetY = event.clientY - boundingRect.top;
+}
+function handleMouseMove(event) {
+   if (!isDragging) return;
+   const newX = event.clientX - offsetX;
+   const newY = event.clientY - offsetY;
+   modmenu.style.left = `${newX}px`;
+   modmenu.style.top = `${newY}px`;
+}
+function handleMouseUp() { isDragging = false; }
 
-   // minimize/maximize window functions
-   function minimizeModMenu() {
-      cdrce('--modmenu-header-height', '5px');
-      cdrce('--maximize-button-left', '165px');
-      cdrce('--modmenu-height', '33px');
-      cdrce('--modmenu-width', '32px');
-      cdrce('--visibility', 'hidden');
-      maximize.className = 'maximize';
-      minimize.className = 'minimize hidden';
-   } function maximizeModMenu() {
-      maximize.className = 'maximize hidden';
-      minimize.className = 'minimize';
-      cdrce('--modmenu-header-height', '8px');
-      cdrce('--maximize-button-left', '0px');
-      cdrce('--modmenu-height', '300px');
-      cdrce('--modmenu-width', '340px');
-      cdrce('--visibility', 'visible');
+// inactive/active toggle handler
+function handleToggle(element) {
+   let ev = element.value;
+   let isActive = (ev === "INACTIVE") ? false : true;
+   if (!isActive) {
+      element.className = 'toggle active';
+      element.value = "ACTIVE";
+   } else {
+      element.className = 'toggle inactive';
+      element.value = "INACTIVE";
    }
+};
 
-   // exploit functions
-   function anonymityMode() {
+// minimize/maximize window functions
+function minimizeModMenu() {
+   // change dimensions
+   cdrce('--modmenu-header-height', '5px');
+   cdrce('--modmenu-height', '28px');
+   cdrce('--modmenu-width', '33px');
 
-   }
+   // change colors
+   mainbordercolor = ggcrv('--main-border-color');
+   headerbordercolor = ggcrv('--header-border-color');
+   headercolor = ggcrv('--header-color');
+   cdrce('--main-border-color', 'var(--minimized-color)');
+   cdrce('--header-border-color', 'var(--minimized-color)');
+   cdrce('--header-color', 'var(--minimized-color)');
 
-   // get modmenu elements
-   var modmenuheader = qrget('.modmenu-header');
-   var modmenu = qrget('.modmenu');
-   var minimize = idget('mini');
-   var maximize = idget('maxi');
+   // change visibility and positions
+   cdrce('--maximize-button-left', '165px');
+   cdrce('--visibility', 'hidden');
 
-   // add drag function listeners
-   modmenuheader.addEventListener('mousedown', handleMouseDown);
-   document.addEventListener('mousemove', handleMouseMove);
-   document.addEventListener('mouseup', handleMouseUp);
+   // hide the minimize element & show the maximize element
+   maximize.className = 'maximize';
+   minimize.className = 'minimize hidden';
+}
+function maximizeModMenu() {
+   // hide the maximize element & show the minimize element
+   maximize.className = 'maximize hidden';
+   minimize.className = 'minimize';
 
-   // process interactions with minimize/maximize buttons
-   minimize.onclick = function () { minimizeModMenu(); }
-   maximize.onclick = function () { maximizeModMenu(); }
+   // change visibility and positions
+   cdrce('--visibility', 'visible');
+   cdrce('--maximize-button-left', '0px');
 
-   // get modmenu feature elements, and process interactions for them
-   var anonymitymode = idget('am-m');
-   anonymitymode.onclick = function() { handleToggle(this); anonymityMode(); }
+   // change colors
+   cdrce('--main-border-color', mainbordercolor);
+   cdrce('--header-border-color', headerbordercolor);
+   cdrce('--header-color', headercolor);
 
+   // change dimensions
+   cdrce('--modmenu-width', '340px');
+   cdrce('--modmenu-height', '300px');
+   cdrce('--modmenu-header-height', '8px');
+}
 
-   // TODO:
-   // Download the CSS and HTML from a hosted file instead of copypasting it
-   // Attack Prediction Lines (Enemy, You)
-   // Grenade Prediction Lines (You)
-   // Attack Nullifier Toggle
-   // Attack Damage Modifier Changer
-   // Info Spoofer
-   // Invincibility
-   // Infinite Boost
-   // No Reload Time
-   // Ban Evade
-   // Kick Evade
-   // Mute Evade
+// exploit functions
+function anonymityMode() {
+
+}
+
+// get modmenu elements
+var modmenuheader = qrget('.modmenu-header');
+var modmenu = qrget('.modmenu');
+var minimize = idget('mini');
+var maximize = idget('maxi');
+
+// add drag function listeners
+modmenuheader.addEventListener('mousedown', handleMouseDown);
+document.addEventListener('mousemove', handleMouseMove);
+document.addEventListener('mouseup', handleMouseUp);
+
+// process interactions with minimize/maximize buttons
+minimize.onclick = function () { minimizeModMenu(); }
+maximize.onclick = function () { maximizeModMenu(); }
+
+// get modmenu feature elements, and process interactions for them
+var anonymitymode = idget('am-m');
+anonymitymode.onclick = function() { handleToggle(this); anonymityMode(); };
+
+// TODO:
+// Download the CSS and HTML from a hosted file instead of copypasting it
+// Attack Prediction Lines (Enemy, You)
+// Grenade Prediction Lines (You)
+// Attack Nullifier Toggle
+// Attack Damage Modifier Changer
+// Info Spoofer
+// Invincibility
+// Infinite Boost
+// No Reload Time
+// Ban Evade
+// Kick Evade
+// Mute Evade
 })();
